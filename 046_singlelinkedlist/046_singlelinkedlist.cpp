d384d337d7e62a9873009312b30f739c57304c55
anandajuliar <anandajulia001@gmail.com>
2024-04-25 17:31:31 +0700

    switch case 1

--- 046_singlelinkedlist/046_singlelinkedlist.cpp
+++ 046_singlelinkedlist/046_singlelinkedlist.cpp
@@ -142,4 +142,12 @@ int main() {
 		cout << "5. Keluar" << endl;
 		cout << "Pilihan: ";
 		cin >> pilihan;
+		switch (pilihan)
+		{
+		case 1:
+			addNode();
+			cout << "Data Berhasil Ditambahkan" << endl;
+			system("pause");
+			system("cls");
+			break;
 
